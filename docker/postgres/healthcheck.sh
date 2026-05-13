7ca97b3db7e20efa19e0ea67ceaa3a4d74af8b37
Tácio <email.tato@gmail.com>
2026-05-13 20:41:52 -0300

    💫 Adiciona Projeto

---
+++ docker/postgres/healthcheck.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -eu
+
+# pg_isready is included in the official postgres image.
+# POSTGRES_USER and POSTGRES_DB are injected by Docker Compose.
+pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"
