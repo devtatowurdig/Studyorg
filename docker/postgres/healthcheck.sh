577938afb42740983559747ce50ceb04cae01e30
Tácio Wurdig <wurdigtm@outlook.com>
2026-05-21 14:49:27 -0300

    🐛 Ajuste para o docker

--- docker/postgres/healthcheck.sh
+++ docker/postgres/healthcheck.sh
@@ -3,4 +3,4 @@ set -eu
 
 # pg_isready is included in the official postgres image.
 # POSTGRES_USER and POSTGRES_DB are injected by Docker Compose.
-pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"
+pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h localhost
